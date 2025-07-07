ef13ca9507620c95aa52eb80ce4dd6c7e63662a6
jiyoungnim <cloundyon31@gmail.com>
2025-07-07 10:19:05 +0900

    📦 Chore: tailwindCSS, alias, router 설정 파일 설치 완료

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@routes", replacement: "/src/routes" },
+      { find: "@zustand", replacement: "/src/zustand" },
+    ],
+  },
+});
